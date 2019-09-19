5c448a3114d228ac11a5e88a5034302b4c48652a
Anthony <anthonyzhu132@gmail.com>
2019-09-19 21:41:30 +0000

    implimented addProperty functionality

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -149,7 +149,7 @@ const getAllProperties = function(options, limit = 10) {
 
   // 6
   return pool.query(queryString, queryParams)
-  .then(res => res.rows);
+    .then(res => res.rows);
 };
 
 exports.getAllProperties = getAllProperties;
@@ -161,9 +161,15 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  return pool.query(`
+  INSERT INTO properties 
+  (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, city, province, post_code)
+
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;
+  `, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.country, property.city, property.province, property.post_code])
+    .then(response => {
+      return response.rows[0];
+    });
 };
 exports.addProperty = addProperty;
